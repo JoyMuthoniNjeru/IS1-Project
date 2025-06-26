d74a5de377e77e4a169c51fdd61588528b5cc990
JoyMuthoniNjeru <joy.njeru23@strathmore.edu>
2025-06-26 16:37:21 +0300

    corrections plus test centre frontend

---
+++ testcentre_mgmt/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class TestCentre(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
