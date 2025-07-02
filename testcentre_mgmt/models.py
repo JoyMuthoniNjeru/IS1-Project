4653900e80badffab8054719fc0118cd6a78701e
JoyMuthoniNjeru <joy.njeru23@strathmore.edu>
2025-07-02 23:09:42 +0300

    still trying to link pages=|

--- testcentre_mgmt/models.py
+++ testcentre_mgmt/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TestCentre(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     capacity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- testcentre_mgmt/views.py
+++ testcentre_mgmt/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .models import TestCentre
 from .forms import TestCentreForm
+from django.http import HttpResponse
+
 
 # View dashboard with up to 3 test centres
+@login_required
 def test_centre_dashboard(request):
-    centres = TestCentre.objects.all()[:3]
-    return render(request, 'testcentre_mgmt/test_centre_dashboard.html', {'centres': centres})
+    if request.user.userprofile.user_type == 'manager':
+        centers = TestCentre.objects.filter(manager=request.user)
+        return render(request, 'testcentre_mgmt/test_centre_dashboard.html', {
+            'centers': centers,
+            'title': 'Your Test Centers'
+        })
+    return HttpResponse("Unauthorized", status=401)
 
 # Add new test centre
-def add_test_centre(request):
+@login_required
+def add_test_center(request):
+    if request.user.userprofile.user_type != 'manager':
+        return HttpResponse("Unauthorized", status=401)
+
     if request.method == 'POST':
         form = TestCentreForm(request.POST)
         if form.is_valid():
-            form.save()
+            center = form.save(commit=False)
+            center.manager = request.user  
+            center.save()
             return redirect('testcentre_mgmt:test_centre_dashboard')
     else:
         form = TestCentreForm()
-    return render(request, 'testcentre_mgmt/form.html', {'form': form, 'title': 'Add Test Centre'})
+    
+    return render(request, 'testcentre_mgmt/form.html', {
+        'form': form,
+        'title': 'Add Test Center'
+    })
 
 # Edit existing test centre
 def edit_test_centre(request, pk):
